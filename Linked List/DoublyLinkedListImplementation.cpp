be3444a6a501d7d3ea230d94280690b5b8d2eca9
ebrahimgamdiwala <ebrahimgamdiwala@gmail.com>
2025-08-03 20:08:05 +0530

    Doubly Linked List Implementation in C++

--- Linked List/DoublyLinkedListImplementation.cpp
+++ Linked List/DoublyLinkedListImplementation.cpp
@@ -4,13 +4,13 @@ using namespace std;
 class Node
 {
 public:
-    int val;
+    int data;
     Node *next;
     Node *prev;
 
     Node(int val)
     {
-        this->val = val;
+        data = val;
         next = prev = nullptr;
     }
 };
@@ -25,9 +25,87 @@ public:
     {
         head = tail = nullptr;
     }
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == nullptr)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == nullptr)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+    void printDLL()
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->data << " <=> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+    void pop_front()
+    {
+        if (head == nullptr)
+        {
+            cout << "Linked List is empty";
+        }
+
+        Node *temp = head;
+        head = head->next;
+        if (head != nullptr)
+        {
+            head->prev = nullptr;
+        }
+        temp->next = nullptr;
+        delete temp;
+    }
+    void pop_back()
+    {
+        if (head == nullptr)
+        {
+            cout << "Linked List is empty";
+        }
+        Node *temp = tail;
+        tail = tail->prev;
+        if (tail != nullptr)
+        {
+            tail->next = nullptr;
+        }
+        tail->prev = nullptr;
+        delete temp;
+    }
 };
 int main()
 {
-
+    DoublyLL dll;
+    dll.push_front(1);
+    dll.push_front(2);
+    dll.push_front(3);
+    dll.push_front(4);
+    dll.push_front(5);
+    dll.push_back(10);
+    dll.pop_front();
+    dll.pop_back();
+    dll.printDLL();
     return 0;
 }
